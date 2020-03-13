2c426be4d0229997e6fae42e24d6756bd80f8548
Haydes <knobea@qq.com>
2020-03-12 22:23:51 -0700

    remove unused files

--- user.sql
+++
@@ -1,8 +0,0 @@
--- 创建用户表
-create table user (
-	id int primary key auto_increment,
-	name varchar(20),
-	password varchar(40)
-)
-
-insert into user values (null, 'jack', '123'),(null,'rose','456');
