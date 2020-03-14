5b20300d987d2a0989b5cdac0b9dda27905b2237
Haydes <knobea@qq.com>
2020-03-14 15:56:45 -0700

    add check out book functions

--- LibAssist-Python/UserController.py
+++ LibAssist-Python/UserController.py
@@ -35,8 +35,6 @@ def validate_user(username, password):
         pwutf8 = bytes(password, 'utf-8')
         return bcrypt.checkpw(pwutf8, pwhash)
 
-
-# Get the ISBN associated with the book a user has borrowed.
 def get_ISBN(username):
     with sqlite3.connect("library.db") as conn:
         cursor = conn.cursor()
@@ -50,3 +48,56 @@ def get_ISBN(username):
             return None
         ISBN = tup[0]
         return ISBN
+
+def borrow_book_byISBN(ISBN, username):
+    with sqlite3.connect("library.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT title, username FROM book where ISBN=?",
+            (ISBN,)
+        )
+
+        tup = cursor.fetchone()
+        if tup is None:
+            return 0    #no book found
+        user = tup[1]    #username from the book table
+        if user is not None:
+            return 1 #other user have got the book
+        else:
+            cursor.execute(
+                "UPDATE book SET username=? where ISBN=?",
+                (username, ISBN,)
+            )
+            cursor.execute(
+                "UPDATE users SET bookISBN=? where username=?",
+                (ISBN, username,)
+            )
+            conn.commit()
+            return  2 #successfully check out
+
+
+def borrow_book_byTitle(title, username):
+    with sqlite3.connect("library.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT title, username FROM book where title=?",
+            (title,)
+        )
+
+        tup = cursor.fetchone()
+        if tup is None:
+            return 0  # no book found
+        user = tup[1]   #username from the book table
+        if user is not None:
+            return 1  # other user have got the book
+        else:
+            cursor.execute(
+                "UPDATE book SET username=? where title=?",
+                (username, title,)
+            )
+            cursor.execute(
+                "UPDATE users SET title=? where username=?",
+                (title, username,)
+            )
+            conn.commit()
+            return 2  # successfully check out
