90cdbe23bb56716138988437a58d8ce7d9c522d0
Haydes <knobea@qq.com>
2020-03-14 16:52:52 -0700

    add show books function

--- LibAssist-Python/BookController.py
+++ LibAssist-Python/BookController.py
@@ -24,13 +24,37 @@ def get_book(ISBN):
             (ISBN,)
         )
 
-    tup = cursor.fetchone()
-    if tup is None:
-        return None
+        tup = cursor.fetchone()
+        if tup is None:
+            return None
 
-    title = tup[0]
-    author = tup[1]
-    pubDate = tup[2]
-    username = tup[3]
-    book = b.Book(ISBN, title, author, pubDate, username)
-    return book
+        title = tup[0]
+        author = tup[1]
+        pubDate = tup[2]
+        username = tup[3]
+        book = b.Book(ISBN, title, author, pubDate, username)
+        return book
+
+# show all the books not checked out by others
+def show_books():
+    with sqlite3.connect("library.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT ISBN, title, author, pubDate, username FROM book where username is null"
+        )
+
+        tups = cursor.fetchall()
+        if tups is None:
+            return None
+        bookList = list()
+        for i in range(len(tups)):
+            ISBN = tups[i][0]
+            title = tups[i][1]
+            author = tups[i][2]
+            pubDate = tups[i][3]
+            username = tups[i][4]
+            book = b.Book(ISBN, title, author, pubDate, username)
+            bookList.append(book)
+
+        return bookList
+
